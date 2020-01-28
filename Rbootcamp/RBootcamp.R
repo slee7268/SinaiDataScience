f4b78e63eaa7fee714bdecf2f53e7ed1dfd1e2fe
ChristopherBellaire <Christopher.Bellaire@icahn.mssm.edu>
2020-01-28 14:23:40 -0500

    Chris

--- Rbootcamp/RBootcamp.R
+++ Rbootcamp/RBootcamp.R
@@ -8,6 +8,9 @@
 #Case-sensitivity and no spaces
 y=3
 print(y)
+#Chris
+x=2
+
 #print(Y) throws an error because we have not set Y to anything
 #Use command + enter or ctrl + enter on Windows to run
 
